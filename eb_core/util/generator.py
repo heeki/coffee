313fa2276711cad89961309b49613f945ca64610
heeki <heekipark@gmail.com>
2020-10-14 13:58:36 -0400

    feat(eb_core): added sf as a target

--- eb_core/util/generator.py
+++ eb_core/util/generator.py
@@ -4,23 +4,20 @@ import json
 import datetime
 
 
-def send_event(event_bus):
+def send_event(event_bus, source, count, detail):
     client = boto3.client("events")
     entries = []
-    for seq in range(10):
+    for seq in range(count):
+        detail["seq"] = seq
         entries.append({
-            "Time": datetime.datetime.now(),
-            "Source": "heeki.custom",
+            "Time": str(datetime.datetime.now()),
+            "Source": source,
             "DetailType": "custom",
-            "Detail": json.dumps({
-                "seq": seq,
-                "key1": "value1",
-                "key2": "value2",
-                "key3": "value3"
-            }),
+            "Detail": json.dumps(detail),
             "EventBusName": event_bus
 
         })
+    print(json.dumps(entries))
     response = client.put_events(
         Entries=entries
     )
@@ -29,9 +26,16 @@ def send_event(event_bus):
 
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument('--bus', required=True, help='EventBridge bus name')
+    ap.add_argument('--bus', required=True, help='bus name')
+    ap.add_argument('--source', required=True, help='source')
+    ap.add_argument('--payload', required=True, help='filename of json payload')
+    ap.add_argument('--count', required=True, help='number of events to generate')
     args = ap.parse_args()
-    send_event(args.bus)
+
+    with open(args.payload) as payload:
+        detail = json.load(payload)
+        detail["metered_date"] = str(datetime.datetime.now())
+    send_event(args.bus, args.source, int(args.count), detail)
 
 
 if __name__ == "__main__":
