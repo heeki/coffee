af885ff2a4a79f6003f98efa2d0a669398c34c10
heeki <heekipark@gmail.com>
2020-12-10 12:04:26 -0500

    feat(api_core): updated authorizer to support v1/v2 event payloads and v2 simple responses

--- api_core/src/authorizer.js
+++ api_core/src/authorizer.js
@@ -1,43 +1,58 @@
-// A simple token-based authorizer example to demonstrate how to use an authorization token 
-// to allow or deny a request. In this example, the caller named 'user' is allowed to invoke 
-// a request if the client-supplied token value is 'allow'. The caller is not allowed to invoke 
-// the request if the token value is 'deny'. If the token value is 'unauthorized' or an empty
-// string, the authorizer function returns an HTTP 401 status code. For any other token value, 
-// the authorizer returns an HTTP 500 status code. 
-// Note that token values are case-sensitive.
+let simple = process.env.SIMPLE_RESPONSE;
 
 exports.handler =  function(event, context, callback) {
     console.log(JSON.stringify(event));
     var token = event.headers.authorization;
     var context = {};
     context.token = token;
+    var resource = '';
+    if (event.version == '1.0' && 'methodArn' in event) {
+        resource = event.methodArn;
+    } else if (event.version == '2.0' && 'routeArn' in event) {
+        resource = event.routeArn;
+    }
     switch (token) {
         case 'allow':
-            callback(null, generatePolicy('user', 'Allow', event.routeArn, context));
+            context.reason = 'generating allow policy'
+            callback(null, generatePolicy('user', 'Allow', resource, context));
             break;
         case 'deny':
-            context.reason = 'deny';
-            callback(null, generatePolicy('user', 'Deny', event.routeArn, context));
+            context.reason = 'generating deny policy';
+            callback(null, generatePolicy('user', 'Deny', resource, context));
             break;
         default:
-            context.reason = 'invalid';
-            callback(null, generatePolicy('user', 'Deny', event.routeArn, context));
+            context.reason = 'invalid token provided';
+            callback(null, generatePolicy('user', 'Deny', resource, context));
     }
 };
 
 var generatePolicy = function(principalId, effect, resource, context) {
     var authResponse = {};
-    authResponse.principalId = principalId;
-    if (effect && resource) {
-        var policyDocument = {};
-        policyDocument.Version = '2012-10-17'; 
-        policyDocument.Statement = [];
-        var statementOne = {};
-        statementOne.Action = 'execute-api:Invoke'; 
-        statementOne.Effect = effect;
-        statementOne.Resource = resource;
-        policyDocument.Statement[0] = statementOne;
-        authResponse.policyDocument = policyDocument;
+    if (simple == "true") {
+        switch (effect) {
+            case 'Allow':
+                authResponse.isAuthorized = true;
+                break;
+            case 'Deny':
+                authResponse.isAuthorized = false;
+                break;
+            default:
+                authResponse.isAuthorized = false;
+                break;
+        }
+    } else {
+        authResponse.principalId = principalId;
+        if (effect && resource) {
+            var policyDocument = {};
+            policyDocument.Version = '2012-10-17'; 
+            policyDocument.Statement = [];
+            var statementOne = {};
+            statementOne.Action = 'execute-api:Invoke'; 
+            statementOne.Effect = effect;
+            statementOne.Resource = resource;
+            policyDocument.Statement[0] = statementOne;
+            authResponse.policyDocument = policyDocument;
+        }
     }
     authResponse.context = context;
     console.log(JSON.stringify(authResponse));
